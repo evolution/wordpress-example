f9be0137efc6e378a3184e4d9b4818825fb28024
Evan Kaufman <evan@digitalflophouse.com>
2014-07-24 09:15:23 -0500

    Updated deploy.rb with bower install

--- lib/capistrano/deploy.rb
+++ lib/capistrano/deploy.rb
@@ -7,5 +7,13 @@ set :domain,        "example.com"
 set :deploy_to,     "/var/www/#{fetch(:domain)}/#{fetch(:stage)}/#{fetch(:branch)}"
 set :wp_path,       "#{release_path}/web/wp"
 
+namespace :deploy do
+  after :updated, :bower_install do
+    on roles(:web) do
+      execute "cd #{release_path} && bower install --config.interactive=false"
+    end
+  end
+end
+
 # Added for testing purposes
 set :repo_url,      "https://github.com/evolution/wordpress-example.git"
